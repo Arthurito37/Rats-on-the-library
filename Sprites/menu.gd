44690de3f205cbc5b13d8e8feebefd3498b30cfe
Arthurito37 <arthursantana364@outlook.com>
2024-11-05 21:46:37 -0300

    Initial commit

---
+++ Sprites/menu.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+
+func _ready():
+	pass
+	
+
+
+func _process(delta):
+	pass
+	
+	
+func _on_iniciar_jogo_pressed():	
+	
+	get_tree().change_scene_to_file("")
+	pass
+	
+
+
+func _on_continuar_jogo_pressed():
+	get_tree().change.change_scene_to_file("")
+	pass #
+	
+	
+func _on_configuracoes_pressed():
+	get_tree().change.change_scene_to_file("")
+	pass #
